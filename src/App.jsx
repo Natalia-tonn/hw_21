34bb772ad3421e10013c25efa17b0a156f3d3747
Natalia-tonn <natali-tonn@web.de>
2024-12-05 18:06:20 +0100

    hw_21

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { submitAnswers } from "./features/questionnaire/questionnaireSlice";
+import Question from "./components/Question";
+import Result  from "./components/Result"
+import styles from "./styles/App.module.css";
+
+
+function App() {
+  const dispatch = useDispatch();
+  const { questions, submitted } = useSelector((state) => state.questionnaire);
+
+  const handleSubmit = () => {
+    dispatch(submitAnswers());
+  };
+
+  return (
+    <div className={styles.appContainer}>
+      <h1>Анкета</h1>
+      {!submitted ? (
+        <div>
+          {questions.map((q) => (
+            <Question key={q.id} {...q} />
+          ))}
+          <button onClick={handleSubmit} className={styles.submitButton}>
+            Отправить
+          </button>
+        </div>
+      ) : (
+        <Result />
+      )}
+    </div>
+  );
+}
+export default App;
+
